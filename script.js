f4199018ad3c90221f2801a8be442dd375e98b38
Darien Bernal <darien.bernal@gmail.com>
2025-02-06 18:45:53 -0500

    Created instances of the Animal class

--- script.js
+++ script.js
@@ -46,15 +46,15 @@ const person2 = {
   console.log("=========================");
   
   // Class Definition
-    class Animal {
-      constructor(eyes, legs, isAwake, isMoving) {
-          //obj.eyes = eyes
-          this.eyes = eyes;
-          this.legs = legs;
-          this.isAwake = isAwake;
-          this.isMoving = isMoving;
-      }
-    }
+    // class Animal {
+    //   constructor(eyes, legs, isAwake, isMoving) {
+    //       //obj.eyes = eyes
+    //       this.eyes = eyes;
+    //       this.legs = legs;
+    //       this.isAwake = isAwake;
+    //       this.isMoving = isMoving;
+    //   }
+    // }
   
   // Instance of a class
   // console.log(animal1);
@@ -67,3 +67,52 @@ const person2 = {
   }
   
   console.log(animals);
+
+  // Class Definition
+class Animal {
+    constructor(eyes, legs, isAwake, isMoving) {
+      //obj.eyes = eyes
+      this.eyes = eyes;
+      this.legs = legs;
+      this.isAwake = isAwake;
+      this.isMoving = isMoving;
+    }
+    sleep() {
+      this.isAwake = false;
+    }
+    wake() {
+      this.isAwake = true;
+    }
+    sit() {
+      this.isMoving = false;
+    }
+    walk() {
+      this.isMoving = true;
+    }
+    speak(sound) {
+      console.log(sound);
+    }
+  
+    toString() {
+      return `This Animal has ${this.eyes} eyes and ${this.legs} legs. It ${this.isAwake ? 'is' : 'is not'} awake, and ${this.isMoving ? 'is' : 'is not'} moving.`;
+    }
+  }
+  
+  // Instance of a class
+  const cat1 = new Animal(2, 4, true, false);
+  console.log(cat1.eyes);
+  
+  const cat2 = new Animal(2, 4, false, false);
+  console.log(cat2.legs);
+  
+  const dog1 = new Animal(2, 4, true, true);
+  dog1.speak("huh");
+  
+  const cow1 = new Animal(2, 4, true, false);
+  cow1.sit();
+  cow1.sleep()
+  console.log(cow1);
+  
+  
+  console.log(cat1.toString());
+  console.log(cat1);
