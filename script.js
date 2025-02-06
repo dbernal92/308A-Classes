e1c305cdedeb52314f6833fddde2c4f3371d5731
Darien Bernal <darien.bernal@gmail.com>
2025-02-06 18:42:02 -0500

    Defined Animal class

--- script.js
+++ script.js
@@ -42,3 +42,28 @@ const person2 = {
 
   person.introduce()
   person2.introduce()
+
+  console.log("=========================");
+  
+  // Class Definition
+    class Animal {
+      constructor(eyes, legs, isAwake, isMoving) {
+          //obj.eyes = eyes
+          this.eyes = eyes;
+          this.legs = legs;
+          this.isAwake = isAwake;
+          this.isMoving = isMoving;
+      }
+    }
+  
+  // Instance of a class
+  // console.log(animal1);
+  
+  const animals = [];
+  
+  for (let i = 0; i < 100; i++) {
+      const animal1 = new Animal(2, 4, true, false);
+      animals.push(animal1)
+  }
+  
+  console.log(animals);
